9b574cfabf6f06ee3618127ba5c3f586cc7ba9c4
Essalution44 <95703535+Essalution44@users.noreply.github.com>
2022-10-02 23:27:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+direction=input("type 'encode' to encrypt type 'decode' to decrypt:\n")
+text=input("type your message\n").lower()
+shift=int(input("type shift number\n"))
+def caeser(direction,text,shift):
+        Endtext=""
+        shift=shift%26
+        if direction == "decode":
+            shift *= -1
+        for letter in text:
+            position=alphabet.index(letter)
+            new_position=position+shift
+            Endtext+=alphabet[new_position]
+        print(f"the {direction}d text is {Endtext}")
+caeser(direction,text,shift)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
